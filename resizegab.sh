4afb5d3a114be03c235308e72ff26e81ca64e492
Linux STB <indiboxes@gmail.com>
2025-02-21 10:04:39 +0700

    Create resizegab.sh
    File ini berfungsi untuk memotong file PDF setengah ukuran, hanya mengambil potongan atas saja (file dipotong horizontal).
    
    File hasil potongan ini menjadi ukuran A5 misalnya, setiap 2 file digabung menjadi 1 file. Jika terdapat jumlah ganjil pada file yang akan dipotong, maka file terakhir akan secara default diubah menjadi ukuran A4.
    
    Setelah semua file yang dipotong tersebut digabung dan sudah di ukuran A4, maka semua file A4 ini dijadikan 1 file PDF lagi sebagai file akhir.

---
+++ resizegab.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Buat direktori sementara
+mkdir -p temp_a5 temp_a4
+
+# Langkah 1: Potong setiap file dan hanya ambil bagian atasnya
+a5_files=()
+for file in *.pdf; do
+  base=$(basename "$file" .pdf)
+  
+  # Potong bagian atas dan simpan
+  top_file="temp_a5/${base}_top.pdf"
+  pdfjam "$file" --trim '0 139.7mm 0 0' --clip true --papersize '{215.9mm,139.7mm}' --outfile "$top_file"
+  a5_files+=("$top_file")
+done
+
+# Langkah 2: Gabungkan setiap dua file bagian atas menjadi satu halaman A4
+a4_files=()
+num_files=${#a5_files[@]}
+for ((i=0; i<num_files; i+=2)); do
+  if [[ -n "${a5_files[i+1]}" ]]; then
+    a4_file="temp_a4/a4_$(printf "%02d" $((i/2+1))).pdf"
+    pdfjam "${a5_files[i]}" "${a5_files[i+1]}" --nup 1x2 --papersize '{210mm,297mm}' --outfile "$a4_file"
+    a4_files+=("$a4_file")
+  else
+    # Jika jumlah file ganjil, file terakhir tetap jadi A4 sendiri
+    a4_file="temp_a4/a4_$(printf "%02d" $((i/2+1))).pdf"
+    pdfjam "${a5_files[i]}" --papersize '{210mm,297mm}' --outfile "$a4_file"
+    a4_files+=("$a4_file")
+  fi
+done
+
+# Langkah 3: Gabungkan semua file A4 menjadi satu file akhir
+pdfjam "${a4_files[@]}" --outfile final_merged.pdf
+
+# Bersihkan direktori sementara
+rm -r temp_a5 temp_a4
+
+echo "Proses selesai! File hasil akhir: final_merged.pdf"
